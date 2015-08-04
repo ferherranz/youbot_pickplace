558091049e56cef5466036e33f8bb32f7027c2ea
ferherranz <ferherranz@gmail.com>
2015-08-04 10:41:53 +0200

    initial commit

---
+++ scripts/pickplace_demo.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2015, Fetch Robotics Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Fetch Robotics Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL FETCH ROBOTICS INC. BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Author: Michael Ferguson
+
+import copy
+import actionlib
+import rospy
+
+from math import sin, cos
+from moveit_python import (MoveGroupInterface,
+                           PlanningSceneInterface,
+                           PickPlaceInterface)
+from moveit_python.geometry import rotate_pose_msg_by_euler_angles
+
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+from control_msgs.msg import PointHeadAction, PointHeadGoal
+from grasping_msgs.msg import FindGraspableObjectsAction, FindGraspableObjectsGoal
+from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from moveit_msgs.msg import PlaceLocation, MoveItErrorCodes
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+# Tools for grasping
+class GraspingClient(object):
+
+    def __init__(self):
+        self.scene = PlanningSceneInterface("base_link")
+        self.pickplace = PickPlaceInterface("arm", "gripper", verbose=True)
+        self.move_group = MoveGroupInterface("arm", "base_link")
+
+        find_topic = "basic_grasping_perception/find_objects"
+        rospy.loginfo("Waiting for %s..." % find_topic)
+        self.find_client = actionlib.SimpleActionClient(find_topic, FindGraspableObjectsAction)
+        self.find_client.wait_for_server()
+
+    def updateScene(self):
+        # find objects
+        goal = FindGraspableObjectsGoal()
+        goal.plan_grasps = True
+        self.find_client.send_goal(goal)
+        self.find_client.wait_for_result(rospy.Duration(5.0))
+        find_result = self.find_client.get_result()
+
+        # remove previous objects
+        for name in self.scene.getKnownCollisionObjects():
+            self.scene.removeCollisionObject(name, False)
+        for name in self.scene.getKnownAttachedObjects():
+            self.scene.removeAttachedObject(name, False)
+        self.scene.waitForSync()
+
+        # insert objects to scene
+        idx = -1
+        for obj in find_result.objects:
+            idx += 1
+            obj.object.name = "object%d"%idx
+            self.scene.addSolidPrimitive(obj.object.name,
+                                         obj.object.primitives[0],
+                                         obj.object.primitive_poses[0],
+                                         wait = False)
+
+        for obj in find_result.support_surfaces:
+            # extend surface to floor, and make wider since we have narrow field of view
+            height = obj.primitive_poses[0].position.z
+            obj.primitives[0].dimensions = [obj.primitives[0].dimensions[0],
+                                            1.5,  # wider
+                                            obj.primitives[0].dimensions[2] + height]
+            obj.primitive_poses[0].position.z += -height/2.0
+
+            # add to scene
+            self.scene.addSolidPrimitive(obj.name,
+                                         obj.primitives[0],
+                                         obj.primitive_poses[0],
+                                         wait = False)
+
+        self.scene.waitForSync()
+
+        # store for grasping
+        self.objects = find_result.objects
+        self.surfaces = find_result.support_surfaces
+
+    def getGraspableCube(self):
+        graspable = None
+        for obj in self.objects:
+            # need grasps
+            if len(obj.grasps) < 1:
+                continue
+            # check size
+            if obj.object.primitives[0].dimensions[0] < 0.05 or \
+               obj.object.primitives[0].dimensions[0] > 0.07 or \
+               obj.object.primitives[0].dimensions[0] < 0.05 or \
+               obj.object.primitives[0].dimensions[0] > 0.07 or \
+               obj.object.primitives[0].dimensions[0] < 0.05 or \
+               obj.object.primitives[0].dimensions[0] > 0.07:
+                continue
+            # has to be on table
+            if obj.object.primitive_poses[0].position.z < 0.5:
+                continue
+            return obj.object, obj.grasps
+        # nothing detected
+        return None, None
+
+    def getSupportSurface(self, name):
+        for surface in self.support_surfaces:
+            if surface.name == name:
+                return surface
+        return None
+
+    def getPlaceLocation(self):
+        pass
+
+    def pick(self, block, grasps):
+        success, pick_result = self.pickplace.pick_with_retry(block.name,
+                                                              grasps,
+                                                              support_name=block.support_surface,
+                                                              scene=self.scene)
+        self.pick_result = pick_result
+        return success
+
+    def place(self, block, pose_stamped):
+        places = list()
+        l = PlaceLocation()
+        l.place_pose.pose = pose_stamped.pose
+        l.place_pose.header.frame_id = pose_stamped.header.frame_id
+
+        # copy the posture, approach and retreat from the grasp used
+        l.post_place_posture = self.pick_result.grasp.pre_grasp_posture
+        l.pre_place_approach = self.pick_result.grasp.pre_grasp_approach
+        l.post_place_retreat = self.pick_result.grasp.post_grasp_retreat
+        places.append(copy.deepcopy(l))
+        # create another several places, rotate each by 90 degrees in yaw direction
+        l.place_pose.pose = rotate_pose_msg_by_euler_angles(l.place_pose.pose, 0, 0, 1.57)
+        places.append(copy.deepcopy(l))
+        l.place_pose.pose = rotate_pose_msg_by_euler_angles(l.place_pose.pose, 0, 0, 1.57)
+        places.append(copy.deepcopy(l))
+        l.place_pose.pose = rotate_pose_msg_by_euler_angles(l.place_pose.pose, 0, 0, 1.57)
+        places.append(copy.deepcopy(l))
+
+        success, place_result = self.pickplace.place_with_retry(block.name,
+                                                                places,
+                                                                scene=self.scene)
+        return success
+
+    # def tuck(self):
+    #     joints = ["shoulder_pan_joint", "shoulder_lift_joint", "upperarm_roll_joint",
+    #               "elbow_flex_joint", "forearm_roll_joint", "wrist_flex_joint", "wrist_roll_joint"]
+    #     pose = [1.32, 1.40, -0.2, 1.72, 0.0, 1.66, 0.0]
+    #     while not rospy.is_shutdown():
+    #         result = self.move_group.moveToJointPosition(joints, pose, 0.02)
+    #         if result.error_code.val == MoveItErrorCodes.SUCCESS:
+    #             return
+
+if __name__ == "__main__":
+    # Create a node
+    rospy.init_node("demo")
+
+    # Make sure sim time is working
+    while not rospy.Time.now():
+        pass
+
+    # Setup clients
+    grasping_client = GraspingClient()
+
+    # Get block to pick
+    while not rospy.is_shutdown():
+        rospy.loginfo("Picking object...")
+        grasping_client.updateScene()
+        cube, grasps = grasping_client.getGraspableCube()
+        if cube == None:
+            rospy.logwarn("Perception failed.")
+            continue
+
+        # Pick the block
+        if grasping_client.pick(cube, grasps):
+            break
+        rospy.logwarn("Grasping failed.")
+
+    # Tuck the arm
+    #grasping_client.tuck()
+
+    # Place the block
+    while not rospy.is_shutdown():
+        rospy.loginfo("Placing object...")
+        pose = PoseStamped()
+        pose.pose = cube.primitive_poses[0]
+        pose.pose.position.z += 0.05
+        pose.header.frame_id = cube.header.frame_id
+        if grasping_client.place(cube, pose):
+            break
+        rospy.logwarn("Placing failed.")
+
+    # Tuck the arm, lower the torso
+    #grasping_client.tuck()
+
